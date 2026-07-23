dcbe5018a60b0805ef009bbc1529064273c8b8f3
shalamay99-rgb <shalamay99@gmail.com>
2026-07-23 13:17:57 +0000

    Deploying to gh-pages from @ shalamay99-rgb/goit-js-hw-10@4727766af4c06e63d3b54b91fec5a9fc36336cc1 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as o}from"./assets/vendor-BbbuE1sJ.js";const m=document.querySelector(".form");m.addEventListener("submit",i=>{i.preventDefault();const s=new FormData(m),t=Number(s.get("delay")),r=s.get("state");new Promise((e,a)=>{setTimeout(()=>{r==="fulfilled"?e(t):a(t)},t)}).then(e=>{o.show({message:`Fulfilled promise in ${e}ms`})}).catch(e=>{o.show({message:`Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=2-snackbar.js.map
